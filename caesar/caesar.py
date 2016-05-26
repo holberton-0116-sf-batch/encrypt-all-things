896a2eea8de6ccdd85808724ebca361f55167e4d
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-26 09:40:19 -0700

    caesar cipher - shift by 3 implementation with corrections

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -4,7 +4,7 @@ import sys
 ''' Caesar cipher implementation in python '''
 
 '''
-#takes first arg
+# takes first arg and prints it
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv[1])
 '''
@@ -15,4 +15,4 @@ for c in sys.argv[1]:
     c = chr(ord(c) + 3)
     out = out + c
 
-print '', out
+print out
