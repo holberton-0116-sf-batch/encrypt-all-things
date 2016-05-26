d7303f9ff54f95e3a82a745f050b1a6bb7ca895c
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-26 09:37:36 -0700

    caesar cipher - shift by 1 implemenation

---
+++ caesar/caesar.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+
+''' Caesar cipher implementation in python '''
+
+'''
+#takes first arg
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv[1])
+'''
+
+out = ''
+
+for c in sys.argv[1]:
+    c = chr(ord(c) + 1)
+    out = out + c
+
+print '', out
